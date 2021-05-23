a57998debe5c014206598d9698d2c5d40096f546
shanekao <shanekao19870615@gmail.com>
2021-05-23 20:32:48 +0800

    add view

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from test_app.views import simple
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('simple/', simple)
+    path('simple/', simple.as_view())
 ]
--- django_api/test_app/views.py
+++ django_api/test_app/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import JsonResponse, response
+from rest_framework.views import APIView
+from django.forms.models import model_to_dict
 
+from .models import TestModel
 # Create your views here.
+
+
+# def simple(request):
+#     # return response.HttpResponse("Welcome")
+#     method = request.method.lower()
+#     if method == 'get':
+#         return JsonResponse({"data": request.method})
+#     elif method == "post":
+#         return JsonResponse({"data": "Add data"})
+#     elif method == "put":
+#         return JsonResponse({"data": "Update data"})
+#     return JsonResponse({"error": "method not allowed"})
+
+class simple(APIView):
+    def post(self, request):
+        new_ = TestModel.objects.create(
+            name=request.data["name"],
+            description=request.data["description"],
+            phone_number=request.data["phone_number"],
+            is_live=request.data["is_live"],
+            amount=request.data["amount"],
+        )
+        return JsonResponse({"data": model_to_dict(new_)})
+
+    def get(self, request):
+        content = TestModel.objects.all().values()
+        return JsonResponse({"data": list(content)})
